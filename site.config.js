74691908ab15f39a56f54a735fff24dce1c46bc2
RachelLYY <778527910@qq.com>
2021-08-29 16:06:33 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: 'addc3495969447a484ab93030bbd3fcc',
+  rootNotionPageId: '0cdc7c0a24124a9a94d9c31585320984',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: null,
+  rootNotionSpaceId: 'addc3495969447a484ab93030bbd3fcc',
 
   // basic site info (required)
   name: 'curetest',
