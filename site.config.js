20694b3bac5ee35087eb6a48a3506022d39fc1a3
RachelLYY <778527910@qq.com>
2021-08-29 15:52:27 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'ab802b3e54344662b9737af240dd6873',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'curetest',
+  domain: 'https://www.notion.so/Project1-ab802b3e54344662b9737af240dd6873',
+  author: 'Reyrey',
 
   // open graph metadata (optional)
   description: 'Example site description',
@@ -18,7 +18,7 @@ module.exports = {
 
   // social usernames (optional)
   twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
+  github: 'rayray',
   linkedin: 'fisch2',
 
   // default notion icon and cover images for site-wide consistency (optional)
